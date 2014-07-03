a3f971217e4f0c02f0f8d4c17deeabd45583e827
Phil Hudson <phil.hudson@iname.com>
2014-07-03 19:46:34 +0100

    Sync

--- ipup2d8.sh
+++ ipup2d8.sh
@@ -1,7 +1,27 @@
-#! bin/bash
+#!/usr/bin/env bash
 
-#This script will periodically check your IP address and publish it to a git page of your choosing.
-#This script will probably only be compatible with GNU-Linux.
+#This script will periodically check your IP address and publish it to a git page
+# of your choosing.
+
+
+################################
+# User configuration section
+################################
+
+IS_NAT="${1:-0}"
+WAN_IP_RESOLVER=ifconfig.me
+# WAN_IP_RESOLVER=ifconfig.me
+# WAN_IP_RESOLVER=icanhazip.com
+# WAN_IP_RESOLVER=ident.me
+# WAN_IP_RESOLVER=ipecho.net/plain
+# WAN_IP_RESOLVER=whatismyip.akamai.com
+# WAN_IP_RESOLVER=tnx.nl/ip
+# WAN_IP_RESOLVER=myip.dnsomatic.com
+# WAN_IP_RESOLVER=ip.appspot.com
+
+################################
+# End user configuration section
+################################
 
 #check for existence of IP output file.
 if [ ! -f ~/ipup2d8/ip ]; then
@@ -16,7 +36,7 @@ if [ ! -f ~/ipup2d8/currentip/currentip ]; then
 	echo
 else
 	touch ~/ipup2d8/currentip/currentip
-	touch ~/ipup2d8/currentip/currentip/README.md
+	touch ~/ipup2d8/currentip/README.md
 	git remote add origin https://github.com/teslasmoustache/currentip.git #CHANGE THIS TO YOUR OWN GIT REPO
 	git push -u origin master
 fi
@@ -25,8 +45,11 @@ fi
 #YOU are using. It's usually wlan0 or eth0. Use ifconfig to find out.
 ip addr show wlp2s0 > ~/ipup2d8/ip
 
-#This line takes the lines containing your IP address from the IP #file that was just created and prints it to another file called #'currentip'.
+#This line takes the lines containing your IP address from the IP 
+#file that was just created and prints it to another file called 
+#'currentip'.
 grep "inet" ~/ipup2d8/ip > ~/ipup2d8/currentip/currentip
 
-#The next step is to upload the currentip file to a Git repository. #You need to CREATE a Git repository on some public site. 
+#The next step is to upload the currentip file to a Git repository. 
+#You need to CREATE a Git repository on some public site. 
 #I'm using Github.
