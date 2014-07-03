d4f7a8331e2755cbb21bb27f74394113ee5b037b
Phil Hudson <phil.hudson@iname.com>
2014-07-03 21:08:20 +0100

    Now a harness for pushing WAN IP address updates to many destinations.

--- ipup2d8.sh
+++ ipup2d8.sh
@@ -1,14 +1,30 @@
 #!/usr/bin/env bash
 
-#This script will periodically check your IP address and publish it to a git page
-# of your choosing.
+# Usage: ipup2d8
+#        ipup2d8 1
+#        ipup2d8 0 en1
+
+# $1: flag (0 or 1) indicating whether we are behind a NAT firewall (1) or not (0)
+# $2: name of local network interface to get WAN IP address from. $1 must be 0.
+
+# This script will periodically check your IP address and pass it to each script
+# you place in the push-ip.d directory.
 
 
 ################################
 # User configuration section
 ################################
 
-IS_NAT="${1:-0}"
+# If you are usually behind a NAT router:
+IS_NAT="${1:-1}"
+# Otherwise:
+#IS_NAT="${1:-0}"
+# The name of the local network interface you want to obtain your WAN IP address
+# from:
+# Be sure to change the name of the network interface to the one that
+# YOU are using. It's usually wlan0 or eth0. Use ifconfig to find out.
+INTERFACE="${2:-eth0}"
+# TODO Randomize
 WAN_IP_RESOLVER=ifconfig.me
 # WAN_IP_RESOLVER=ifconfig.me
 # WAN_IP_RESOLVER=icanhazip.com
@@ -23,33 +39,45 @@ WAN_IP_RESOLVER=ifconfig.me
 # End user configuration section
 ################################
 
-#check for existence of IP output file.
-if [ ! -f ~/ipup2d8/ip ]; then
-	echo
+IPUP2D8_DIR="${HOME}/.ipup2d8"
+IPUP2D8_FILE="${IPUP2D8_DIR}/ip"
+TASKS_DIR="${IPUP2D8_DIR}/push-ip.d"
+
+mkdir -p "$IPUP2D8_DIR"
+cd "$IPUP2D8_DIR"
+
+touch "$IPUP2D8_FILE"
+
+OLD_IP=`cat "$IPUP2D8_FILE"`
+
+IP="" # store IP
+
+if [ "$IS_NAT" = 0 ]; then
+    case `uname` in
+        Linux)
+            IP=`ifconfig -a "$INTERFACE" | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`
+            ;;
+
+        FreeBSD|OpenBSD)
+            IP=`ifconfig "$INTERFACE" | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}'`
+            ;;
+
+        SunOS)
+            IP=`ifconfig -a "$INTERFACE" | grep inet | grep -v '127.0.0.1' | awk '{ print $2} '`
+            ;;
+
+        Darwin)
+            IP=`ifconfig -u "$INTERFACE" | grep -v 'inet 127.0.0.1' | sed -n 's/\s*inet \(\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}\) .*/\1/p'`
+            ;;
+    esac
 else
-	touch ~/ipup2d8/ip
+    IP=`curl "$WAN_IP_RESOLVER"`
 fi
 
-mkdir ~/ipup2d8/currentip
-#check for existence of file containing current or last known IP.
-if [ ! -f ~/ipup2d8/currentip/currentip ]; then
-	echo
-else
-	touch ~/ipup2d8/currentip/currentip
-	touch ~/ipup2d8/currentip/README.md
-	git remote add origin https://github.com/teslasmoustache/currentip.git #CHANGE THIS TO YOUR OWN GIT REPO
-	git push -u origin master
-fi
+[ "$IP" = "$OLD_IP" ] && exit 0
 
-#Be sure to change the name of the network interface to the one that
-#YOU are using. It's usually wlan0 or eth0. Use ifconfig to find out.
-ip addr show wlp2s0 > ~/ipup2d8/ip
+echo "$IP" > "$IPUP2D8_FILE"
 
-#This line takes the lines containing your IP address from the IP 
-#file that was just created and prints it to another file called 
-#'currentip'.
-grep "inet" ~/ipup2d8/ip > ~/ipup2d8/currentip/currentip
-
-#The next step is to upload the currentip file to a Git repository. 
-#You need to CREATE a Git repository on some public site. 
-#I'm using Github.
+for TASK in `ls "$TASKS_DIR"`; do
+    "$TASK" "$IP"
+done
