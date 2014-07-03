9c9d3bf10fe0982cda866af1b6545807113eea0f
teslasmoustache <teslas_moustache@riseup.net>
2014-07-02 21:30:27 -0500

    check if IP file exists

--- ipup2d8.sh
+++ ipup2d8.sh
@@ -3,6 +3,13 @@
 #This script will periodically check your IP address and publish it to a git page of your choosing.
 #This script will probably only be compatible with GNU-Linux.
 
+#check for existence of IP output file.
+if [ ! -f ~/ipup2d8/ip ]; then
+	echo 0
+else
+	touch ~/ipup2d8/ip
+fi
+
 ip addr show wlp2s0 > ~/ipup2d8/ip
 #you will probably need to change the above line. 
 #Use ifconfig to find out what the name of the interface is. (e.g. wlan0, or eth0 most commonly)
